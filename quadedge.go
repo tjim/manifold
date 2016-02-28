98cd7db306aaf2cec86451584e9459b1c86d8da3
Trevor Jim <tjim@mac.com>
2016-02-28 08:46:22 -0500

    More uniform random point selection

--- quadedge.go
+++ quadedge.go
@@ -439,11 +439,17 @@ func draw(e0 *Edge, file string) {
 
 func main() {
 	bigTri := Ngon(3, 1e4)
-	for i := 0; i < 1000; i++ {
-		x, y := math.Sincos(2.0 * math.Pi * rand.Float64())
-		radius := rand.Float64() * 100.0
-		x, y =  radius * x, radius * y
-		x, y = x + 150.0, y + 105.0
+	for i := 0; i < 10000; i++ {
+		var x, y float64
+		for {
+			x, y = rand.Float64(), rand.Float64() // in [0,1)
+			x, y = x - 0.5, y - 0.5 // in [-0.5, 0.5)
+			if x*x + y*y < 0.5 * 0.5 { // in unit circle
+				x, y = x * 200.0, y * 200.0
+				x, y = x + 150.0, y + 105.0
+				break
+			}
+		}
 		InsertSite(&Point2D{x, y}, bigTri)
 	}
 	draw(bigTri, nextfile())
